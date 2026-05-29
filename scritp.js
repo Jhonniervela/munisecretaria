d90856ed9b7072c648620db7b889826ff6d7d337
Brayan Vela <108531761+Jhonniervela@users.noreply.github.com>
2026-05-29 13:18:49 -0500

    Implement service worker registration
    Add service worker registration script to scritp.js

--- scritp.js
+++ scritp.js
@@ -1,1 +1,12 @@
-
+   <script>
+  if ('serviceWorker' in navigator) {
+    // Registramos el sw.js que está en la raíz
+    navigator.serviceWorker.register('./sw.js')
+      .then(reg => {
+        console.log('LlactaYachay: Service Worker Activo', reg.scope);
+      })
+      .catch(err => {
+        console.error('LlactaYachay: Error de registro', err);
+      });
+  }
+</script>
